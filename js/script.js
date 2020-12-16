833e1165c8ed48deaf05d800f0dafb4717716dbb
Daniel <danielpellegrini87@gmail.com>
2020-12-16 18:10:03 +0100

    added comments + fixed capitalized lowercase

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
+//  Array
+var surnameList = ['bianchi', 'rossi', 'duzioni', 'balsano', 'verdi'];
+console.log(surnameList);
+
+// User input
 var surname = prompt('What\'s your surname?');
+surname = surname.toLowerCase(); //capital lowercase
 document.getElementById('surname').innerHTML = surname + '\'s position is: ';
 
+// Loop while unfilled surname
 while (surname.length <= 0) {
   surname = prompt('What\'s your surname?');
 }
 
-var surnameList = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
-console.log(surnameList);
 
-surnameList.push(surname);
-surnameList.sort();
+surnameList.push(surname); // Pushing the user inside the Array
+surnameList.sort(); // Sorting the Array
 
 for (var i = 0; i < surnameList.length; i++) {
   document.getElementById('surname-list').innerHTML +=('<li>' + surnameList[i] + '</li>');
@@ -20,6 +25,8 @@ console.log(surnameList.indexOf(surname));
 var position = surnameList.indexOf(surname) + 1;
 document.getElementById('surname-position').innerHTML = position;
 
+
+// Ordinal indicator for position
 if (position === 1) {
   document.getElementById('surname-position').innerHTML = position + 'st';
 } else if (position === 2) {
